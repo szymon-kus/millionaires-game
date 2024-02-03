14170c24d0801cd7a1e02ec79e34f600ff7c10b2
Szymon Kuś <144013915+szymon-kus@users.noreply.github.com>
2024-02-03 23:18:35 +0100

    Add files via upload

---
+++ Milionerzy/Milionerzy/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Milionerzy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraMilionerzy gra = new GraMilionerzy();
+            gra.RozpocznijGre();
+        }
+    }
+}
---
+++ Milionerzy/Milionerzy/Uzytkownik.cs
@@ -0,0 +1,46 @@
+﻿class Uzytkownik
+{
+    private string directoryPath;
+
+    public string Imie { get; private set; }
+    public int Wynik { get; private set; }
+    public DateTime Data { get; private set; }
+
+    public Uzytkownik()
+    {
+        directoryPath = Environment.CurrentDirectory;
+        WprowadzImie();
+        Data = DateTime.Now;
+    }
+
+    private void WprowadzImie()
+    {
+        Console.Write("Podaj swoje imię: ");
+        Imie = Console.ReadLine();
+    }
+
+    public void UstawWynik(int wynik)
+    {
+        Wynik = wynik;
+    }
+
+    public void ZapiszWynikDoPliku()
+    {
+        string sciezka = Path.Combine(directoryPath, "zwyciezcyMilionerow.txt");
+
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(sciezka, true))
+              
+            {
+                writer.WriteLine($"{Imie}, {Wynik}, {Data.ToString("yyyy-MM-dd HH:mm:ss")}");
+            }
+
+            Console.WriteLine("Wynik został zapisany do pliku.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Wystąpił błąd podczas zapisywania wyniku do pliku: {ex.Message}");
+        }
+    }
+}
